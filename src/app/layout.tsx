c2c8da7d84b802c2643dda91fe31880dbf8e4658
Paul O'Connell <p_ollie@hotmail.com>
2026-01-07 11:56:28 -0800

    Feature: added toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HeaderServer from "@/components/Header/HeaderServer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <HeaderServer />
         {children}
+          <Toaster position="top-right" />
       </body>
     </html>
   );
